957640b43fdcdc97d5e0d1db414db1f9c7edd5d2
Jennings Liu <jennings.liu@sap.com>
2018-06-20 17:47:13 +0800

    add more example

--- chap09/main.go
+++ chap09/main.go
@@ -9,6 +9,9 @@ func main() {
         if i%2 == 0 {
             continue
         }
-        fmt.Printf("%d ", i)
-    }
+        fmt.Printf("%d\n ", i)
+		}
+		for no, i := 10, 1; i <= 10 && no <= 19; i, no = i+1, no+1 { //multiple initialisation and increment
+			fmt.Printf("%d * %d = %d\n", no, i, no*i)
+	}
 }
