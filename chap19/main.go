b134e59a262933bb75183a8e97feae66cee36b0b
Jennings Liu <jenningsloy318@gmail.com>
2018-06-26 22:28:38 +0800

    add chap19

---
+++ chap19/main.go
@@ -0,0 +1,92 @@
+package main
+
+import "fmt"
+
+type Describer interface {  
+    Describe()
+}
+type Person struct {  
+    name string
+    age  int
+}
+
+func (p Person) Describe() { // 使用值接受者实现  
+    fmt.Printf("%s is %d years old\n", p.name, p.age)
+}
+
+type Address struct {
+    state   string
+    country string
+}
+
+func (a *Address) Describe() { // 使用指针接受者实现
+    fmt.Printf("State %s Country %s\n", a.state, a.country)
+}
+
+// 多个接口
+
+type SalaryCalculator interface {  
+    DisplaySalary()
+}
+
+type LeaveCalculator interface {  
+    CalculateLeavesLeft() int
+}
+
+type Employee struct {  
+    firstName string
+    lastName string
+    basicPay int
+    pf int
+    totalLeaves int
+    leavesTaken int
+}
+
+func (e Employee) DisplaySalary() {  
+    fmt.Printf("%s %s has salary $%d", e.firstName, e.lastName, (e.basicPay + e.pf))
+}
+
+func (e Employee) CalculateLeavesLeft() int {  
+    return e.totalLeaves - e.leavesTaken
+}
+
+func main() {  
+    var d1 Describer
+    p1 := Person{"Sam", 25}
+    d1 = p1
+    d1.Describe()
+    p2 := Person{"James", 32}
+    d1 = &p2
+    d1.Describe()
+
+    var d2 Describer
+    a := Address{"Washington", "USA"}
+
+    /* 如果下面一行取消注释会导致编译错误：
+       cannot use a (type Address) as type Describer
+       in assignment: Address does not implement
+       Describer (Describe method has pointer
+       receiver)
+    */
+    //d2 = a
+
+    d2 = &a // 这是合法的
+    // 因为在第 22 行，Address 类型的指针实现了 Describer 接口
+    d2.Describe()
+    // 多个接口
+
+        e := Employee {
+        firstName: "Naveen",
+        lastName: "Ramanathan",
+        basicPay: 5000,
+        pf: 200,
+        totalLeaves: 30,
+        leavesTaken: 5,
+    }
+    var s SalaryCalculator = e
+    s.DisplaySalary()
+    var l LeaveCalculator = e
+    fmt.Println("\nLeaves left =", l.CalculateLeavesLeft())
+
+
+}
